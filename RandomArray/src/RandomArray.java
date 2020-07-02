666896c66dce80373328ee89644a8d5b11740098
DaleRoyFall <complete079@gmail.com>
2020-07-02 16:15:18 +0300

    Use logger

--- RandomArray/src/RandomArray.java
+++ RandomArray/src/RandomArray.java
@@ -1,7 +1,11 @@
+import java.util.logging.Logger;
+
 public class RandomArray {
+    private static final Logger LOGGER = Logger.getLogger(RandomArray.class.getName());
+
     public static void main(String args[]) {
         ArrayOperations arrayOperations = new ArrayOperations(new int[]{1,2,3},new int[] {1,3,4});
 
-        System.out.println(arrayOperations.getNumber());
+        LOGGER.info(arrayOperations.getNumber().toString());
     }
 }
